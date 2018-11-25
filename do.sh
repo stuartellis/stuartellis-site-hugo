ccf7fdf753e39148b5fce88c71b1f774605f5da9
Stuart Ellis <stuart@stuartellis.name>
2018-11-25 16:03:29 +0000

    Add utility shell script

---
+++ do.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+case $1 in
+  clean)
+    if [ -d public ]; then rm -r public; fi
+    ;;
+  deploy)
+    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" hugo-toolbox /var/local/deploy.sh
+    ;;
+  serve)
+    docker run --rm -p 1313:1313 --mount "type=bind,source=${PWD},destination=/var/local" hugo-toolbox
+    ;;
+  shell)
+    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" hugo-toolbox /bin/sh
+    ;;
+  *)
+    echo "Not a valid command"
+    ;;
+esac
