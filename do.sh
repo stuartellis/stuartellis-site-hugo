5e68965a21287521859ae4115061e10b55a51e1d
Stuart Ellis <stuart@stuartellis.name>
2018-12-09 16:24:31 +0000

    Update utility script

--- do.sh
+++ do.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
+PROJECT_NAME=`basename ${PWD}`
+BUILDER_IMAGE_VERSION=`cat ./Dockerfile | grep "version" | grep -oe "[0-9]\+[.][0-9]\+[.][0-9]\+"`
+BUILDER_IMAGE_ID="$PROJECT_NAME-builder:$BUILDER_IMAGE_VERSION"
+
 case $1 in
   clean)
     if [ -d public ]; then rm -r public; fi
     ;;
   deploy)
-    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" hugo-toolbox /var/local/deploy.sh
+    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" $BUILDER_IMAGE_ID /var/local/deploy.sh
+    ;;
+  info)
+    echo "Project name: ${PROJECT_NAME}"
+    echo "Docker builder image version: ${BUILDER_IMAGE_VERSION}"
     ;;
   serve)
-    docker run --rm -p 1313:1313 --mount "type=bind,source=${PWD},destination=/var/local" hugo-toolbox
+    docker run --rm -p 1313:1313 --mount "type=bind,source=${PWD},destination=/var/local" $BUILDER_IMAGE_ID
     ;;
   setup)
-    docker build .
+    docker build . -t $BUILDER_IMAGE_ID
     ;;
   shell)
-    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" hugo-toolbox /bin/sh
+    docker run --rm -it --mount "type=bind,source=${PWD},destination=/var/local" --mount "type=bind,source=${HOME}/.aws,destination=/root/.aws,readonly" $BUILDER_IMAGE_ID /bin/sh
     ;;
   *)
     echo "$1 is not a valid command"
     ;;
 esac
+
