4c19985577ae463e3a0e4934c0cb06b2f6b51791
Stuart Ellis <stuart@stuartellis.name>
2019-05-21 05:58:24 +0100

    Set public access for S3 on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 hugo -v 
-s3deploy -source=public/ -region=eu-west-2 -bucket=www.stuartellis.name
+s3deploy -source=public/ -public-access -region=eu-west-2 -bucket=www.stuartellis.name -distribution-id=E3FWVVWISZCBK9
